f99b3984fe6d79485c2d1fd39aca36745469ae6e
Arcaneax <nix_vork90@hotmail.com>
2016-09-26 11:55:22 +0200

    Crawler + lidt ekstra :D

---
+++ Crawler/Crawler/Posting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler {
+    class Posting {
+
+        List<int> posting = new List<int>();
+
+        public Posting (int docId) {
+            posting.Add(docId);
+        }
+
+        public Posting (List<int> posting) {
+            this.posting = posting;
+        }
+
+        public List<int> getList {
+            get { return posting; }
+        }
+        
+    }
+}
---
+++ Crawler/Crawler/QueryProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler {
+    class QueryProcessor {
+
+        public void processQuery(String query) {
+            
+        }
+
+        public List<int> intersect(Posting post1, Posting post2) {
+            List<int> matchingDocIds = new List<int>();
+
+            post1.getList.Sort();
+            post2.getList.Sort();
+
+            int i = 0, j = 0;
+            while (i < post1.getList.Count && post2.getList.Count < j) {
+                if (post1.getList[i].Equals(post2.getList[j])) {
+                    matchingDocIds.Add(post1.getList[i]);
+                    i++;
+                    j++;
+                } else if (post1.getList[i] < post2.getList[j]) {
+                    i++;
+                } else if (post1.getList[i] > post2.getList[j]) {
+                    j++;
+                }
+            }
+            return matchingDocIds;
+        }
+
+        public List<int> union(Posting post1, Posting post2) {
+            List<int> matchingDocIds = new List<int>();
+
+            foreach (int docId in post1.getList) {
+                if (!matchingDocIds.Contains(docId)) {
+                    matchingDocIds.Add(docId);
+                }
+            }
+
+            foreach (int docId in post2.getList) {
+                if (!matchingDocIds.Contains(docId)) {
+                    matchingDocIds.Add(docId);
+                }
+            }
+
+            return matchingDocIds;
+        }
+
+        /*public List<int> complement(Posting post) {
+
+        }*/
+
+    }
+}
