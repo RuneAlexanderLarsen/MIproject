b511874846a18c6b17e36a1a2de3c4130fcf1549
Pikachew <nvork13@student.aau.dk>
2016-10-03 12:00:52 +0200

    Sammensætning v2

---
+++ SearchEngine/SearchEngine/Posting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler {
+    class Posting {
+
+        List<Tuple<int, int>> posting = new List<Tuple<int, int>>();
+
+        public Posting (int docId, int termFrequency) {
+            posting.Add(new Tuple<int, int>(docId, termFrequency));
+        }
+
+        public Posting (List<Tuple<int, int>> posting) {
+            this.posting = posting;
+        }
+
+        public List<Tuple<int, int>> getList {
+            get { return posting; }
+        }
+        
+    }
+}
