271508e12981b174c6d6283154c69a8decfb0268
RuneAlexanderLarsen <rala13@student.aau.dk>
2016-10-17 10:36:40 +0200

    pageranker

---
+++ SearchEngine/SearchEngine/PageRanker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows;
+using System.Net;
+using System.IO;
+
+namespace SearchEngine
+{
+    public class PageRanker
+    {
+
+        public static void PageRankMain(string[] urls)
+        {
+            int[] idURL = new int[1000];
+            idURL = ConvertUrlsToInt(urls);
+
+            for (int i = 0; i >= 1000; i++)
+            {
+                LinksInURL(idURL[i]);
+            }
+                
+            place the NotFiniteNumberException in pagearray
+            
+            int[,] PageArray = new int[1000,1000];
+            
+        }
+
+        public static int GetPageRank(string url)
+        {
+            int rankPart = 0;
+
+            int[,] PageArray = new int[1000,2];
+
+            rankPart = numberOfLinks / totalCites;
+
+
+
+            return rank;
+        }
+
+    }
+
+}
--- SearchEngine/SearchEngine/Program.cs
+++ SearchEngine/SearchEngine/Program.cs
@@ -1,16 +1,20 @@
-﻿using stopword;
+﻿using SearchEngine;
+using stopword;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Crawler {
-    class Program {
+namespace Crawler
+{
+    class Program
+    {
         private static int MAX_PAGES = 1000;
 
-        static void Main(string[] args) {
-            String result = "";
+        static void Main(string[] args)
+        {
+            /*String result = "";
 
             //Crawler
             Spider spider = new Spider();
@@ -22,10 +26,10 @@ namespace Crawler {
 
                 //Removal of stopwords and Tokenise
                 Stop stop = new Stop();
-                result = stop.initialize(result);
-            }
+                result = stop.initialize(result);*/
 
-            //Console.WriteLine(result);
+            int i = PageRanker.GetPageRank("https://www.microsoft.com");
+            Console.WriteLine(i);
             Console.ReadLine();
         }
     }
